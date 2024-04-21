d65312dac56501b33da4bae84039f8b32a66f76d
Asmaa saleh <92892342+AsmaaSal@users.noreply.github.com>
2024-04-21 20:56:32 +0200

    Update API_Ops.php

--- API_Ops.php
+++ API_Ops.php
@@ -1,7 +1,6 @@
 <?php
 
-
-$url_1='https://online-movie-database.p.rapidapi.com/actors/list-born-today?month='.$_GET['month'].'&day='.$_GET['day'];
+$url_1 = 'https://online-movie-database.p.rapidapi.com/actors/list-born-today?month=' . $_GET['month'] . '&day=' . $_GET['day'];
 
 $curl_1 = curl_init();
 
@@ -19,55 +18,36 @@ curl_setopt_array($curl_1, [
     ],
 ]);
 
-$response_1=curl_exec($curl_1);
-$responsejson_1=json_decode($response_1,true);
-//$err = curl_error($curl_1);
+$response1 = curl_exec($curl_1);
+$response1= json_decode($response1);
 curl_close($curl_1);
-/*
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
-}*/
 
+foreach ($response1 as $actor) {
+    
+    $actor_id = basename(parse_url($actor, PHP_URL_PATH));
+    $url_2 = "https://online-movie-database.p.rapidapi.com/actors/get-bio?nconst=" . $actor_id;
+    $curl_2 = curl_init();
 
+    curl_setopt_array($curl_2, [
+        CURLOPT_URL => $url_2,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => [
+            "X-RapidAPI-Host: online-movie-database.p.rapidapi.com",
+            "X-RapidAPI-Key: 3ef6d553femsh1fece224baf6632p192d58jsnf7474723fa3b"
+        ],
+    ]);
+    $response2 = curl_exec($curl_2);
+    $response2 = json_decode($response2);
+    if (!empty($respons2)) {
 
-
-
-if(!empty($responsejson_1['data']))
-{
-    foreach ($responsejson_1['data'] as $actor) {
-        $actor_id=$actor['nconst'];
-        $url_2 = "https://online-movie-database.p.rapidapi.com/actors/get-bio?nconst=" . $actor_id;
-        $curl_2 = curl_init();
-
-        curl_setopt_array($curl_2, [
-            CURLOPT_URL => $url_2,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => [
-                "X-RapidAPI-Host: online-movie-database.p.rapidapi.com",
-                "X-RapidAPI-Key: 3ef6d553femsh1fece224baf6632p192d58jsnf7474723fa3b"
-            ],
-        ]);
-        $response = curl_exec($curl_2);
-        $responsejson = json_decode($response,true);
-        if (!empty($response_json['data'])) {
-
-            echo "Actor: " . $actor['primaryName'] . "<br>";
-        }
-
-
-        curl_close($curl_2);
-
+        echo "Actor: " . $response2->name . "<br>";
     }
+    curl_close($curl_2);
 }
-else
-{
-    echo "No actors found for today.";
-}
+
 ?>
