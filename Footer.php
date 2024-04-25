5f9d3b37b4e08742db24e27732608ce771af3c47
MaryamSakr <102592232+MaryamSakr@users.noreply.github.com>
2024-04-25 17:16:58 +0200

    edit header and footer

---
+++ Footer.php
@@ -0,0 +1,14 @@
+<header>
+    <link rel="stylesheet" href="footer.css">
+</header>
+<footer>
+    <nav>
+        <ul>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms of Service</a></li>
+            <li><a href="#">FAQ</a></li>
+            <!-- Add more links as needed -->
+        </ul>
+    </nav>
+    <p>&copy; <?php echo date("Y"); ?> Registeration form. All rights reserved.</p>
+</footer>
---
+++ Header.php
@@ -0,0 +1,12 @@
+<header>
+    <link rel="stylesheet" href="Header.css">
+    <h1>Registeration Form</h1>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</header>
